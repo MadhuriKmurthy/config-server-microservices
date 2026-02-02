b92a5f43e009f98963b17a0bc314b700f4a7f950
MadhuriKMurthy <madhuri.k.aithal@gmail.com>
2026-02-01 23:13:13 -0600

    Adding configs of all 3 apps into spring cloud

--- configserver/src/main/java/com/microservices/configserver/ConfigserverApplication.java
+++ configserver/src/main/java/com/microservices/configserver/ConfigserverApplication.java
@@ -4,6 +4,18 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
 
+/**
+ * To access the configs use:
+ * http://localhost:8071/accounts/default
+ * http://localhost:8071/accounts/qa
+ * http://localhost:8071/accounts/prod
+ * http://localhost:8071/cards/default
+ * http://localhost:8071/cards/qa
+ * http://localhost:8071/cards/prod
+ * http://localhost:8071/loans/default
+ * http://localhost:8071/loans/qa
+ * http://localhost:8071/loans/prod
+ */
 @SpringBootApplication
 @EnableConfigServer
 public class ConfigserverApplication {
